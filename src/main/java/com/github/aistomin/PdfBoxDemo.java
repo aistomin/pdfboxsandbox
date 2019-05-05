a38ec6fd1365adfb23a468d5b41808210f9bfd16
aistomin <andrej.istomin.ikeen@gmail.com>
2019-05-05 06:27:14 +0200

    The PDF reading logic is extracted to PdfReader.java.

--- src/main/java/com/github/aistomin/PdfBoxDemo.java
+++ src/main/java/com/github/aistomin/PdfBoxDemo.java
@@ -1,12 +1,7 @@
 package com.github.aistomin;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
-import java.util.UUID;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.util.PDFTextStripper;
-import org.apache.pdfbox.util.PDFTextStripperByArea;
 
 /**
  * Created by aistomin on 2019-05-01.
@@ -23,103 +18,25 @@ public final class PdfBoxDemo {
      * @throws Exception If something goes wrong :)
      */
     public static void main(final String[] args) throws Exception {
-        printFileInfo(resourceFile("not-encrypted.pdf"), null);
-        printFileInfo(resourceFile("encrypted.pdf"), "bach");
-//        printFileInfo(resourceFile("path_to_your_file"), "your_password");
-    }
-
-    /**
-     * Prints the details of the file to console.
-     *
-     * @param file     The PDF file.
-     * @param password Optional password in case the file is encrypted.
-     * @throws Exception If something goes wrong :)
-     */
-    private static void printFileInfo(
-        final File file, final String password
-    ) throws Exception {
-        final PDDocument doc = PDDocument.load(file);
-        System.out.println(
-            String.format("****** INFORMATION ABOUT %s ******", file)
-        );
-        System.out.println(
-            String.format("IS ENCRYPTED: %b", doc.isEncrypted())
-        );
-        if (doc.isEncrypted()) {
-            System.out.println(
+        final String pdf = "encrypted.pdf";
+        final URL resource = PdfBoxDemo.class.getClassLoader()
+            .getResource(pdf);
+        if (resource == null) {
+            throw new Exception(
                 String.format(
-                    "The document %s is encrypted. Let's try to decrypt it.",
-                    file.getAbsolutePath()
-                )
-            );
-            decrypt(doc, password);
-            System.out.println(
-                String.format(
-                    "The document %s is decrypted.", file.getAbsolutePath()
+                    "The resource with the name %s is not found.", pdf
                 )
             );
         }
+        final File file = new File(resource.getFile());
+        final String text = new PdfReader(file, "bach").read();
         System.out.println(
-            String.format("CONTENT OF THE FILE:\n%s", loadText(doc))
+            String.format(
+                "CONTENT OF THE FILE %s:\n%s", file.getAbsolutePath(), text
+            )
         );
         System.out.println(
             "***********************************************\n\n"
         );
-    }
-
-    /**
-     * Decrypt the PDF document.
-     *
-     * @param doc      The document.
-     * @param password The password.
-     * @throws Exception If something goes wrong :)
-     */
-    private static void decrypt(
-        final PDDocument doc, final String password
-    ) throws Exception {
-        if (password == null) {
-            throw new NullPointerException(
-                "Password is required for the encrypted document."
-            );
-        }
-        doc.decrypt(password);
-        doc.setAllSecurityToBeRemoved(true);
-        doc.save(
-            String.format("target/%s.pdf", UUID.randomUUID().toString())
-        );
-    }
-
-    /**
-     * Load the content of the PDF document.
-     *
-     * @param doc The document.
-     * @return The text.
-     * @throws IOException If the file is not readable.
-     */
-    private static String loadText(final PDDocument doc) throws IOException {
-        final PDFTextStripper stripper = new PDFTextStripperByArea();
-        stripper.setSortByPosition(true);
-        return new PDFTextStripper().getText(doc);
-    }
-
-    /**
-     * Get the file of the resource file.
-     *
-     * @param name Resource file name.
-     * @return The {@link PDDocument} instance.
-     * @throws Exception If the resource is not found.
-     */
-    private static File resourceFile(final String name)
-        throws Exception {
-        final URL resource = PdfBoxDemo.class.getClassLoader()
-            .getResource(name);
-        if (resource == null) {
-            throw new Exception(
-                String.format(
-                    "The resource with the name %s is not found.", name
-                )
-            );
-        }
-        return new File(resource.getFile());
     }
 }
---
+++ src/main/java/com/github/aistomin/PdfReader.java
@@ -0,0 +1,86 @@
+package com.github.aistomin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+import org.apache.pdfbox.util.PDFTextStripperByArea;
+
+/**
+ * Created by aistomin on 2019-05-05.
+ * <p>
+ * Class that can read the text from the PDF file.
+ */
+@SuppressWarnings("WeakerAccess")
+public final class PdfReader {
+
+    /**
+     * The PDF file.
+     */
+    private final File pdf;
+
+    /**
+     * Optional password in case the file is encrypted.
+     */
+    private final String password;
+
+    /**
+     * Ctor.
+     *
+     * @param pdf      The PDF file.
+     * @param password Optional password in case the file is encrypted.
+     */
+    public PdfReader(final File pdf, final String password) {
+        this.pdf = pdf;
+        this.password = password;
+    }
+
+    /**
+     * Reads the content of the PDF file.
+     *
+     * @throws Exception If something goes wrong :)
+     */
+    public String read() throws Exception {
+        final PDDocument doc = PDDocument.load(this.pdf);
+        if (doc.isEncrypted()) {
+            decrypt(doc, this.password);
+        }
+        return loadText(doc);
+    }
+
+    /**
+     * Decrypt the PDF document.
+     *
+     * @param doc      The document.
+     * @param password The password.
+     * @throws Exception If something goes wrong :)
+     */
+    private void decrypt(
+        final PDDocument doc, final String password
+    ) throws Exception {
+        if (password == null) {
+            throw new NullPointerException(
+                "Password is required for the encrypted document."
+            );
+        }
+        doc.decrypt(password);
+        doc.setAllSecurityToBeRemoved(true);
+        doc.save(
+            String.format("target/%s.pdf", UUID.randomUUID().toString())
+        );
+    }
+
+    /**
+     * Load the content of the PDF document.
+     *
+     * @param doc The document.
+     * @return The text.
+     * @throws IOException If the file is not readable.
+     */
+    private String loadText(final PDDocument doc) throws IOException {
+        final PDFTextStripper stripper = new PDFTextStripperByArea();
+        stripper.setSortByPosition(true);
+        return new PDFTextStripper().getText(doc);
+    }
+}
